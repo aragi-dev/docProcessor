2ba2ee8fab722528d836ec13f3cb4876ffccdd46
aldo-dev <omatu.dev@gmail.com>
2025-06-15 15:50:47 -0600

    feat: :fire: reomove filess

--- src/layouts/MainLayout.vue
+++ src/layouts/MainLayout.vue
@@ -1,11 +1,9 @@
 <template>
   <main class="grid gap-4 p-4">
-    <SpeedInsights />
     <MainNav />
     <router-view />
   </main>
 </template>
 <script setup lang="ts">
 import MainNav from "@/components/MainNav.vue";
-import { SpeedInsights } from "@vercel/speed-insights/next"
 </script>
