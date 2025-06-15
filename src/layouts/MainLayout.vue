15858a7b986bbb3f822d005d581542583b360581
aldo-dev <omatu.dev@gmail.com>
2025-06-15 15:44:11 -0600

    feat: :heavy_plus_sign: add dependence

--- src/layouts/MainLayout.vue
+++ src/layouts/MainLayout.vue
@@ -1,9 +1,11 @@
 <template>
   <main class="grid gap-4 p-4">
+    <SpeedInsights />
     <MainNav />
     <router-view />
   </main>
 </template>
 <script setup lang="ts">
 import MainNav from "@/components/MainNav.vue";
+import { SpeedInsights } from "@vercel/speed-insights/next"
 </script>
