0c3738b862d203ec79579734aa12852e4e87d8df
Aldo <119747217+aragi-dev@users.noreply.github.com>
2025-06-17 17:18:10 -0600

    Update HomeView.vue

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,8 +1,9 @@
 <template>
   <main class="grid">
-    <div class="m-auto grid grid-flow-row gap-4 p-4">
-      <Icon name="pdf" css="fill-zinc-300" />
-      <Icon name="exel" css="fill-zinc-300" />
+    <div class="m-auto grid grid-flow-col gap-4 p-4">
+      <Icon name="doc" css="fill-zinc-500" size="30" />
+      <p class="text-md text-zinc-400">a</p>
+      <Icon name="doc" css="fill-zinc-500" size="30" />
     </div>
   </main>
 </template>
