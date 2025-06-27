14d5eb185c09e0a0976604280d81b283b8c99bce
Aldo <119747217+aragi-dev@users.noreply.github.com>
2025-06-26 21:36:48 -0600

    Update MainNav.vue

--- src/components/MainNav.vue
+++ src/components/MainNav.vue
@@ -1,8 +1,8 @@
+x
 <script setup lang="ts">
 import Icon from "@/components/Icon.vue";
 import { useRouter } from "vue-router";
 import { useAuthStore } from "@/services/auth";
-import { Role } from "@/utils/enums/Role";
 
 const router = useRouter();
 const auth = useAuthStore();
@@ -14,26 +14,10 @@ const logout = () => {
 
 </script>
 <template>
-  <nav class="flex items-center text-center text-white w-full px-6">
-    <section>
-      <select class="bg-zinc-900 text-zinc-500 outline-none border-none font-bold">
-        <option class="mx-auto font-bold bg-zinc-900 text-zinc-500" selected value="es">ES</option>
-        <option class="mx-auto font-bold bg-zinc-900 text-zinc-500" value="en">EN</option>
-      </select>
-    </section>
-    <section class="grid grid-flow-col m-auto p-2 rounded-xl shadow-sm shadow-black">
-      <router-link v-if="auth.hasRole([Role.ADMIN, Role.USER])" to="/home"
-        class="py-2 px-3 rounded-lg transition-transform hover:cursor-pointer hover:scale-125">
-        <Icon name="home" size="24" css="fill-zinc-500" />
-      </router-link>
-      <router-link v-if="auth.hasRole([Role.ADMIN])" to="/user"
-        class="py-2 px-3 rounded-lg transition-transform hover:cursor-pointer hover:scale-125">
-        <Icon name="user" size="24" css="fill-zinc-500" />
-      </router-link>
-    </section>
-    <section>
-      <Icon @click="logout" name="logut" size="24"
-        css="fill-zinc-500 transition-color hover:cursor-pointer hover:scale-125 hover:fill-red-400" />
-    </section>
+  <nav class="fixed top-0 left-0 right-0 z-50 grid-flow-col items-center p-4 grid">
+    <Icon @click="logout" name="logout" size="24"
+      css="fill-zinc-500 transition hover:cursor-pointer hover:scale-125 hover:fill-red-400 rotate-180 mr-auto" />
+    <Icon name="config" size="24"
+      css="fill-zinc-500 transition hover:cursor-pointer hover:scale-125 hover:7 active:opacity-40 ml-auto" />
   </nav>
 </template>
