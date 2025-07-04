eca1bb2fc8273f6793a4ac055155517fa658fa64
Aldo <119747217+aragi-dev@users.noreply.github.com>
2025-07-04 10:08:10 -0600

    Update BaseInput.vue

--- src/components/BaseInput.vue
+++ src/components/BaseInput.vue
@@ -1,13 +1,13 @@
 <template>
-  <div class="grid gap-1">
-    <label :for="props.name" :class="['text-gray-400 ml-1', props.labelClass]">
+  <div class="grid">
+    <label :for="props.name" :class="['text-gray-400 ml-1 font-semibold', props.labelClass]">
       {{ props.label }}
     </label>
     <div class="relative">
       <input :type="props.type" :name="props.name" :id="props.name"
         :value="props.type === 'number' && typeof props.modelValue === 'number' ? String(props.modelValue) : props.modelValue"
         @input="onInput" @blur="props.onBlur"
-        :class="['py-2 px-4 border-1 border-zinc-700 text-gray-400 rounded-2xl bg-zinc-800 focus:outline-none focus:border-zinc-500 w-full', props.inputClass]" />
+        :class="['py-2 px-4 rounded-2xl text-white shadow-lg border border-white/20 backdrop-blur-sm bg-gradient-to-br from-white/10 to-white/5 transition w-full duration-300 focus:outline-none', props.inputClass]" />
       <span v-if="props.touched && props.error" class="absolute right-0 top-0 flex h-3 w-3 pointer-events-none">
         <span class="absolute inline-flex h-full w-full animate-ping rounded-full bg-red-400 opacity-75"></span>
         <span class="relative inline-flex h-3 w-3 rounded-full bg-red-500"></span>
